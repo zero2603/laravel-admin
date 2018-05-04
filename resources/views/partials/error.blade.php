f42f15629a7cd05d89e718adad276e689206207e
zero2603 <20153713@student.hust.edu.vn>
2018-05-04 12:04:53 +0700

    first commit

---
+++ resources/views/partials/error.blade.php
@@ -0,0 +1,13 @@
+@if (count($errors)) 
+    
+    <ul>
+
+        @foreach($errors->all() as $error) 
+          
+            <li>{ { $error } }</li>
+
+        @endforeach 
+
+    </ul>
+
+@endif 
